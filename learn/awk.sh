8474592199943ef1f0932e6460d8633324146bb8
collins-ruto <collinsruto48@gmail.com>
2023-03-30 15:25:50 +0300

    ¯\_(ツ)_/¯

--- learn/awk.sh
+++ learn/awk.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# test.txt contains an multiple/array of string
+# piping array output to awk and print second item 
 cat test.txt | awk '{print $2}'
+# use -F, to separate the string by a , cut the preceeding space
 echo "one, two, three, four" | awk -F, '{print $3}' | cut -c2-
+# separate by :
 echo "get only word: hello" | awk -F: '{print $2}' | cut -c2-
