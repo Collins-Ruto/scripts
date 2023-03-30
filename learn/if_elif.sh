1da775113506f503b64d208afdc3357652a58b2e
collins-ruto <collinsruto48@gmail.com>
2023-03-30 15:25:40 +0300

    ¯\_(ツ)_/¯

--- learn/if_elif.sh
+++ learn/if_elif.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# if elif statements
+# compare first parameter to collins. ,, ignore capitalization from parameters
 if [ ${1,,} = collins ]; then
     echo "Hello Collins. Welcome"
 elif [ ${1,,} = help ]; then
