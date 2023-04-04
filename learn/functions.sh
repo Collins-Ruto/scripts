ea29ff1ae67139148591a70feeac312ac6dde988
collins-ruto <collinsruto48@gmail.com>
2023-04-04 19:15:57 +0300

    ¯\_(ツ)_/¯

---
+++ learn/functions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+showuptime(){
+    local up=$(uptime -p | cut -c4-)
+    local since=$(uptime -s)
+    cat << EOF
+-----
+This machine has been up for ${up}
+It has been running from ${since}
+-----
+EOF
+}
+
+showuptime
