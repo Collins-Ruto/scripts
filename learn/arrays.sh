2774fd70f8c1e355e2dd10b7097a595ddc383199
collins-ruto <collinsruto48@gmail.com>
2023-05-23 00:29:46 +0300

    ¯\_(ツ)_/¯

---
+++ learn/arrays.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+MY_LIST=(one two three four)
+
+for i in MY_LIST; echo $i; end
---
+++ learn/piping.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# compare two values if they are equal. then print out the exit code
+[ hello = hello ] ; echo $status # returns 0 for success. 1 is for error
+
+# compare two values if they are not equal. then print out the exit code
+[ hello != hello ] ; echo $status # returns 1 for error. 0 is for success
+
+
